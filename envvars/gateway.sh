acd54d52352703729fac4244805c5b101681bf6e
Mitesh The Mouse <44154255+miteshget@users.noreply.github.com>
2022-12-01 16:02:02 +0530

    Update gateway.sh

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -8,14 +8,23 @@
 #
 #export TERMINAL_TAB='split'
 
+# Disable OpenShift Console, this is not appropriate for Red Hat Summit as it
+# would point to the wrong cluster.
+#export CONSOLE_URL='http://0.0.0.0:10083'
+
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
-#export DEFAULT_PAGE='workshop'
+export DEFAULT_PAGE='dashboard'
 
 # Flags to explicitly enable/disable specific components
-#ENABLE_CONSOLE='false'
-#ENABLE_DASHBOARD='false'
-#ENABLE_SLIDES='false'
-#ENABLE_TERMINAL='false'
+ENABLE_CONSOLE='false'
+ENABLE_DASHBOARD='true'
+ENABLE_SLIDES='true'
+SLIDES_DIR='/opt/app-root/slides'
+ENABLE_TERMINAL='true'
 #ENABLE_WEBDAV='false'
-#ENABLE_WORKSHOP='true'
+ENABLE_WORKSHOP='true'
+APPLICATION_NAME='bookbag'
+WORKSHOP_NAME='bookbag'
+AUTH_USERNAME='bob'
+AUTH_PASSWORD='r3dh4t1!'
